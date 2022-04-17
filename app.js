d776adf2eaba76ed25d652c255fc01c163e1435b
Bucky Collins <bucky.collins.67@gmail.com>
2022-04-17 15:51:41 -0700

    Coding Challenge and Solution for 4-17-2022.  Used PREP method to work through challenge.  Followed MayanWolf's directions on Twitch.

---
+++ app.js
@@ -0,0 +1,32 @@
+// In this kata you will create a function that takes in a list and returns a list with the reverse 
+// order.
+
+// Examples (Input -> Output)
+// * [1, 2, 3, 4]  -> [4, 3, 2, 1]
+// * [9, 2, 0, 7]  -> [7, 0, 2, 9]
+
+function reverseList(list) {
+    return (list.reverse())  //originally I logged the result to the console, but apparently code wars wanted it returned.
+}
+
+// ------------------------------------------------------------
+// MayanWolf, who has gone through the previous cohort, goes through a "P.R.E.P" process.  I'll try and
+// emulate that here.
+
+// P - (Parameters) - Will this always take a set of positive intergers?  Will we ever be given an empty
+// array?
+
+// R - (Return) - Our intention is to return an array, in reverse order.  As noted above, console.log is
+// not returning it, and was infact the wrong solution.
+
+// E - (Examples) - Three examples of what is considered a proper answer.
+// * [1, 2, 3, 4]  -> [4, 3, 2, 1]
+// * [9, 2, 0, 7]  -> [7, 0, 2, 9]
+// * [2, 4, 6] -> [6, 4, 2]
+
+// P - (Pseudocode) - In plain english our code will...
+// Take an array named 'list'
+// Use a built in method in javaScript to reverse that array called 'list'
+// return that reversed array called 'list'
+
+// ------------------------------------------------------------
